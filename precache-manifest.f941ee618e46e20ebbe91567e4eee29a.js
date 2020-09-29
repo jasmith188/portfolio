411dda33365841b3b54cfa6a7ba394ff97ff3b6c
James Smith <jasmith.188@gmail.com>
2020-09-29 12:23:12 -0400

    Updates

---
+++ precache-manifest.f941ee618e46e20ebbe91567e4eee29a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7302843af0acd02d4e9b486762448ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "329e2d69e5dda27ad1f1",
+    "url": "/static/css/main.97bb2382.chunk.css"
+  },
+  {
+    "revision": "3ed528fd15541b4c954a",
+    "url": "/static/js/2.7ac0f38a.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.7ac0f38a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "329e2d69e5dda27ad1f1",
+    "url": "/static/js/main.8e0e1265.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "c693abaf09860727cc519ba54def8020",
+    "url": "/static/media/profile.c693abaf.jpeg"
+  }
+]);
