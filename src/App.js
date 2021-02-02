a369c0ea75a266ea4c96485e37e0d69a9341f011
James Smith <jasmith.188@gmail.com>
2021-02-02 16:10:10 -0500

    fix some errors

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import NavBar from './components/NavBar/NavBar';
-import About from './components/About/About';
-import Main from './components/Main/Main';
-import Project from './components/Project/Project';
-import data from './components/Data';
-// import CardPage from './components/CardPage';
-import ContactForm from './components/Contact/Contact';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import About from './Components/About';
+import Resume from './Components/Resume';
+import Contact from './Components/Contact';
+import Testimonials from './Components/Testimonials';
+import Portfolio from './Components/Portfolio';
 
-function App() {
+const App = () => {
+  const [resumeData, setResumeData] = useState({});
+
+  useEffect(() => {
+    fetch('/resumeData.json')
+      .then((res) => res.json())
+      .then((data) => {
+        setResumeData(data);
+      });
+  }, []);
   return (
     <div className="App">
-      {/* <NavBar name={data.name} contactEmail={data.contactEmail} /> */}
-      <NavBar />
-      <Main name={data.landingPageName} authorImage={data.landingPageImage} />
-      <About
-        paragraph={data.landingPagePara}
-        paragraph2={data.landingPagePara2}
-      />
-      {/* <CardPage /> */}
-      <Project projects={data.projects} />
-      <ContactForm />
+      <Header data={resumeData.main} />
+      <About data={resumeData.main} />
+      <Resume data={resumeData.resume} />
+      <Portfolio data={resumeData.portfolio} />
+      <Testimonials data={resumeData.testimonials} />
+      <Contact data={resumeData.main} />
+      <Footer data={resumeData.main} />
     </div>
   );
-}
+};
 
 export default App;
