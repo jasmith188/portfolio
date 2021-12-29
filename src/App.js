ecbab44fdf43ddd86f49771c3b16f8829f407a10
jasmith188 <jasmith.188@gmail.com>
2021-12-29 11:39:00 -0500

    update fix

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import './App.css';
 import About from './components/About';
 import Contact from './components/Contact';
 import Header from './components/Header';
-// import Navbar from './components/Navbar';
 import Projects from './components/Projects';
 
 function App() {
   return (
     <div className="app">
       <Header />
-      {/* <Navbar /> */}
       <About />
       <Projects />
       <Contact />
